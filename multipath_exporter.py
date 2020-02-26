55920bfeaaef6dbf32e9a640486fc71413676278
Sergei Mikhailov <s.mikhaylov@rtk-dc.ru>
2020-02-26 11:38:43 +0300

    Pylint fixes

--- multipath_exporter.py
+++ multipath_exporter.py
@@ -17,6 +17,11 @@ else:
     import subprocess
 
 
+class MultipathdExporterException(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
 def validate_host():
     try:
         uid = os.getuid()
@@ -28,16 +33,16 @@ def validate_host():
         multipath_version_line = re.match(
             '^multipath-tools.*$', multipath_help_stdout, re.M).group(0)
         multipath_version = multipath_version_line.split(' ')[1].replace('v', '')
-        logging.debug("Multipath version is <%s>" % multipath_version)
+        logging.debug("Multipath version is <%s>", multipath_version)
         if semver.compare(multipath_version, multipath_min_version) >= 0 and \
            semver.compare(multipath_version, multipath_max_version) <= 0:
             return True
         else:
-            logging.error("Multipath version <%s> is unsupported, must be between <%s> and <%s>" %
-                          (multipath_version, multipath_min_version, multipath_max_version))
+            logging.error("Multipath version <%s> is unsupported, must be between <%s> and <%s>",
+                          multipath_version, multipath_min_version, multipath_max_version)
             return False
     except BaseException as err:
-        logging.error("Cannot check multipath version: %s" % err)
+        logging.error("Cannot check multipath version: %s", err)
         return False
 
 
@@ -48,7 +53,7 @@ def load_multipath_data():
             ['multipathd', 'show', 'maps', 'json'], stderr=subprocess.STDOUT, timeout=cmd_timeout)
         src_data = json.loads(multipathd_output)
     except BaseException as err:
-        logging.error("Cannot get valid data from multipathd: %s" % err)
+        logging.error("Cannot get valid data from multipathd: %s", err)
     return src_data
 
 
@@ -74,7 +79,7 @@ def get_luns_state(multipath_data):
         }
         registry = raw_metrics_to_registried(metrics_object)
     except BaseException as err:
-        logging.error("Cannot get LUN states: %s" % err)
+        logging.error("Cannot get LUN states: %s", err)
     return registry
 
 
@@ -89,10 +94,10 @@ def raw_metrics_to_registried(raw_metrics):
                 try:
                     metric_gauge.labels(*metric['labels']).set(metric['value'])
                 except BaseException as err:
-                    logging.warning("Cannot set metric <%s>: %s" % (metric, err))
+                    logging.warning("Cannot set metric <%s>: %s", metric, err)
         except BaseException as err:
-            logging.warning("Cannot process metric <%s> with data <%s>: %s" %
-                            (metric_name, metric_data, err))
+            logging.warning("Cannot process metric <%s> with data <%s>: %s",
+                            metric_name, metric_data, err)
     return registry
 
 
@@ -102,11 +107,11 @@ def update_metrics(registry):
         if multipath_data:
             registry.register(get_luns_state(multipath_data))
     except BaseException as err:
-        logging.error("Cannot update metrics: %s" % err)
+        logging.error("Cannot update metrics: %s", err)
 
 
-def log_fatal(msg):
-    logging.fatal(msg)
+def log_fatal(msg, *args, **kwargs):
+    logging.fatal(msg, *args, **kwargs)
     sys.exit(1)
 
 
@@ -119,14 +124,14 @@ def main():
     try:
         main_registry = prom.CollectorRegistry()
     except BaseException as err:
-        log_fatal("Cannot init prometheus collector: %s" % err)
+        log_fatal("Cannot init prometheus collector: %s", err)
 
     update_metrics(main_registry)
 
     try:
         prom.start_http_server(listen_port, registry=main_registry)
     except BaseException as err:
-        log_fatal("Cannot start HTTP server, exiting: %s" % err)
+        log_fatal("Cannot start HTTP server, exiting: %s", err)
 
     try:
         while True:
@@ -136,16 +141,19 @@ def main():
     except KeyboardInterrupt:
         log_fatal("Exiting on keyboard interrupt")
     except BaseException as err:
-        log_fatal("Main loop crashed, exiting: %s" % err)
+        log_fatal("Main loop crashed, exiting: %s", err)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     try:
         parser = argparse.ArgumentParser(description='Multipath LUN metrics exporter')
-        parser.add_argument("--listen-port", default=9684, help="Port to listen, int, default 9684")
-        parser.add_argument("--cmd-timeout", default=2.0, help="Timeout for shell calls, float, default 2.0")
-        parser.add_argument("--collect-interval", default=60.0, help="Metrics update interval, float, default 60.0")
+        parser.add_argument("--listen-port", default=9684,
+                            help="Port to listen, int, default 9684")
+        parser.add_argument("--cmd-timeout", default=2.0,
+                            help="Timeout for shell calls, float, default 2.0")
+        parser.add_argument("--collect-interval", default=60.0,
+                            help="Metrics update interval, float, default 60.0")
         parser_args = parser.parse_args()
 
         cmd_timeout = parser_args.cmd_timeout
@@ -154,5 +162,5 @@ if __name__ == "__main__":
         multipath_min_version = '0.4.6'
         multipath_max_version = '0.7.9'
     except BaseException as err:
-        log_fatal("Cannot init variables, exiting: %s" % err)
+        log_fatal("Cannot init variables, exiting: %s", err)
     main()
